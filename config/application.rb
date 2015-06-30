69ca7fcab0bf09f2b2198bd3176336dc859942b7
Jason Watson <jswatson0@gmail.com>
2015-06-30 18:09:25 -0500

    update gemfile and app.rb for assets

--- config/application.rb
+++ config/application.rb
@@ -22,8 +22,5 @@ module WatsonPortfolio
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
-    config.serve_static_files = true
-    config.action_dispatch.x_sendfile_header = nil
   end
 end
