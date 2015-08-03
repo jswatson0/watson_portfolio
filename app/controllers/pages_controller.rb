b86972535623952f41c27daf2a732cca4685f0ca
Jason Watson <jswatson0@gmail.com>
2015-08-03 11:02:44 -0500

    use send_file to display downloadable resume.pdf

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,6 @@ class PagesController < ApplicationController
   end
 
   def resume
-
+    send_file Rails.root.join("public", "assets", "downloads", "jason_watson_resume.pdf"), :type => 'application/pdf', :disposition => 'inline'
   end
 end
